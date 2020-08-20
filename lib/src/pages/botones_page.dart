e7f726db184f37f2004b44915b333ce95c8dc4db
Alan Barrera <aalan.eduardo@gmail.com>
2020-08-19 19:34:22 -0500

    19agost

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -7,15 +7,16 @@ class BotonesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Stack(
-      children: <Widget>[
-        _fondoApp(),
-        SingleChildScrollView(
-          child: Column(
-            children: <Widget>[_titulos()],
-          ),
-        )
-      ],
-    ));
+          children: <Widget>[
+            _fondoApp(),
+            SingleChildScrollView(
+              child: Column(
+                children: <Widget>[_titulos()],
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: _bottomNavigatorBar(context));
   }
 
   Widget _fondoApp() {
@@ -75,5 +76,23 @@ class BotonesPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget _bottomNavigatorBar(BuildContext context) {
+    return Theme(
+        data: Theme.of(context).copyWith(
+            canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+            primaryColor: Colors.pinkAccent,
+            textTheme: Theme.of(context).textTheme.copyWith(
+                caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0)))),
+        child: BottomNavigationBar(items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today, size: 30.0), title: Container()),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bubble_chart, size: 30.0), title: Container()),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.supervised_user_circle, size: 30.0),
+              title: Container()),
+        ]));
   }
 }
